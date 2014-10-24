58ca290c33d25f50de29a4a889c4347be56cb01c
Jesse Swartzendruber <jswartzendruber@enova.com>
2014-10-23 20:28:13 -0500

    little bit better.:

--- randomizer.rb
+++ randomizer.rb
@@ -1,24 +1,31 @@
-# Set up base array
-participants = []
-ARGV.each do |a|
-  participants.push(a)
+class Person
+  attr_accessor :is_gifted, :name, :recipient
 end
 
-# Create arrays to use
-gifters = participants.shuffle
-recipients = participants.shuffle
-assignments = {}
-start = 0
-
-# Assign
-gifters.each do |giver|
-  if giver != recipients[start]
-    assignments[giver] = recipients[start]
-    start = start + 1
+def set_up_array
+  @participants = []
+  ARGV.shuffle.each do |a|
+    p = Person.new
+    p.name = a
+    p.is_gifted = false
+    @participants.push(p)
   end
 end
 
-# Print out
-assignments.each do |giver,taker|
-  puts "#{giver} -> #{taker}"
+def assign_recipients
+  place = 0
+  while place < @participants.length
+    @participants[place].recipient = @participants[place-1]
+    place += 1
+  end
 end
+
+def print_results
+  @participants.each do |p|
+    puts "#{p.name} -> #{p.recipient.name}"
+  end
+end
+
+set_up_array
+assign_recipients
+print_results
